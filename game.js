437e92ace8fa13c3e2c0abb63595b3b4f8bc32d2
= <necipmna2542@gmail.com>
2025-01-02 15:33:03 +0300

    Update letter and add script structure

---
+++ game.js
@@ -0,0 +1,26 @@
+const letter1 = document.getElementById("letter1");
+const letter2 = document.getElementById("letter2");
+const letter3 = document.getElementById("letter3");
+const letter4 = document.getElementById("letter4");
+const letter5 = document.getElementById("letter5");
+const pred_input = document.getElementById("prediction_input");
+const submit_button = document.getElementById("submit_button");
+const reset_button = document.getElementById("reset_button");
+const score_p = document.getElementById("score");
+const heart1 = document.getElementById("heart1");
+const heart2 = document.getElementById("heart2");
+const heart3 = document.getElementById("heart3");
+
+function prediction(text){
+
+}
+
+function reset(){
+
+}
+
+submit_button.addEventListener("click" ()=>{
+    
+
+})
+reset_button.addEventListener("click",reset);
